afb22a18b9dcf66c875b5591a700713f884d59f8
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-08-18 22:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,18 +16,40 @@ function showIssues(json) {
 }
 
 function createIssue() {
+  const title = document.getElementById('title').value
+  const body = document.getElementById('body').value
+   const postData = {
+    title : title,
+    body : body
+  }
+   fetch(`${fork}/issues`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+     body: JSON.stringify(postData)
+  }).then(res => res.json()).then(json => getIssues())
+
 }
 
 function showResults(json) {
+   $('#results').append(`<h4>Forked Successfully!</h4><a href="${json.url}">${json.full_name}</a>`)
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  //use fetch to fork it!	  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`,{
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showResults(json))
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  const token = ''
+  return token
 }
